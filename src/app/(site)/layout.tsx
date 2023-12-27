1b892cf3e70cd9ec944a53a8cd1aaa5b802fa3a3
saumyaranjan.parida-at-252540970514 <saumyaranjan.parida@raweng.com>
2023-12-27 22:30:41 +0530

    Added Login and Signup Page For NoteEase

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      {/* <Header /> */}
+      {/* <Header />  -->  Instead of this we are using seperate children container for the same*/}  
       {children}
     </main>
   );
