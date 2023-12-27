f48b1d561455afdb3f66fcdc89e4dcb7f599f54e
saumyaranjan.parida-at-252540970514 <saumyaranjan.parida@raweng.com>
2023-12-27 12:09:21 +0530

    Implemented Landing Page For NoteEase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  console.log(":::::::::", db)
+  // console.log(":::::::::", db)
   return (
     <html lang="en">
       <body className={inter.className}>
