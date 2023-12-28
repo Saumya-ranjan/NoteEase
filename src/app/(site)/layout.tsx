83bfa554ac906381a68be7cee399df7628185967
saumyaranjan.parida-at-252540970514 <saumyaranjan.parida@raweng.com>
2023-12-28 11:43:33 +0530

    Implemented Header in NoteEase

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,10 +1,10 @@
-// import Header from '@/components/landing-page/header';
+import Header from '@/components/landing/header';
 import React from 'react';
 
 const HomePageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      {/* <Header />  -->  Instead of this we are using seperate children container for the same*/}  
+      <Header />
       {children}
     </main>
   );
